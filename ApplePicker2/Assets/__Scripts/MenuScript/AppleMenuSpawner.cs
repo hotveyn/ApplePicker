91f559e61a2a609a1ee4fc0b5b7e48994d24dcfe
hotveyn <hotveyn@gmail.com>
2021-07-02 21:10:05 +0300

    little patch
    Improved apple spawn in mane menu and fix bag with main music

--- ApplePicker2/Assets/__Scripts/MenuScript/AppleMenuSpawner.cs
+++ ApplePicker2/Assets/__Scripts/MenuScript/AppleMenuSpawner.cs
@@ -17,12 +17,16 @@ public class AppleMenuSpawner : MonoBehaviour
         InvokeRepeating("SpawnApple", 0.5f, 0.5f);
         InvokeRepeating("SpawnApple", 1.0f, 0.7f);
         InvokeRepeating("SpawnApple", 1.5f, 0.9f);
+        InvokeRepeating("SpawnApple", 2f, 1.1f);
+        InvokeRepeating("SpawnApple", 2.5f, 1.4f);
+        InvokeRepeating("SpawnApple", 3f, 1.6f);
+        InvokeRepeating("SpawnApple", 3.5f, 1.8f);
         
     }
 
     void SpawnApple()
     {
-        Vector3 spawnPoint = new Vector3(Random.Range(-2f, 2.5f), 4f, -7f);
+        Vector3 spawnPoint = new Vector3(Random.Range(-2.2f, 2.7f), 4f, -7f);
         
         if (UnityEngine.Random.Range(0, 100) < chanceToDropGoldenApple)
         {
